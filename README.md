0d01407e53029553be182f965f0032df7eb1ad79
Lim Moon <lim.moon.95@gmail.com>
2020-10-11 12:38:52 -0700

    common child lcs solution

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 * [Problem Description](https://www.welcomekakao.com/learn/courses/30/lessons/42892)
 * [Solution](vacationRoute/Solution.java)
 * [Approach](https://limmoon.github.io/2020/09/27/post-1/)
+
+## Common Child (LCS)
+* [Problem Description](https://www.hackerrank.com/challenges/common-child/problem)
+* [Solution](commonChild/Solution.java)
+* [Approach](https://limmoon.github.io/2020/09/27/post-3/)
---
+++ commonChild/Solution.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the commonChild function below.
+    static int commonChild(String s1, String s2) {
+        int lcs[][] = new int[s1.length()+1][s2.length()+1];
+        for(int i = 0; i <= s1.length(); i++){
+            for(int j = 0; j <= s2.length(); j++){
+                if(i==0 || j==0)
+                    lcs[i][j]=0;
+                else if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    lcs[i][j] = lcs[i-1][j-1]+1;
+                }
+                else{
+                    lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+                }
+            }
+        }
+        return lcs[s1.length()][s2.length()];
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = scanner.nextLine();
+
+        String s2 = scanner.nextLine();
+
+        int result = commonChild(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
